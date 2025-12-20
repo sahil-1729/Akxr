3a595779e0a2d8975828c8ab42f1efdde42e6d85
Sah!l <97158384+sahil-1729@users.noreply.github.com>
2025-12-20 20:15:39 +0530

    feat: completed and total task counter

--- Project/script.js
+++ Project/script.js
@@ -1,3 +1,11 @@
+
+const totalTask = document.querySelector('.total-task-counter')
+totalTask.textContent = 0
+
+const completedTask = document.querySelector('.completed-task-counter')
+completedTask.textContent = 0
+
+
 const btn = document.querySelector('.create-task-button')
 
 const taskList = document.querySelector('.task-list')
@@ -38,21 +46,39 @@ btn.addEventListener("click", function (e) {
 
     addTask.value = ""
 
+    const totalTask = document.querySelector('.total-task-counter')
+    const cntTotalTask = Number(totalTask.textContent)
+    totalTask.textContent = cntTotalTask + 1
 })
 
-// delete task 
+// delete task and mark completed 
 taskList.addEventListener('click', function (e) {
     if (e.target.className === 'task-delete-button') {
         const li = e.target.parentNode
         li.parentNode.removeChild(li)
+
+        const totalTask = document.querySelector('.total-task-counter')
+        const cntTotalTask = Number(totalTask.textContent)
+        totalTask.textContent = cntTotalTask - 1
     } else if (e.target.className === "task-checkbox") {
         const task = e.target.nextSibling
 
         if (e.target.checked) {
+            task.setAttribute('class', 'completed-task')
             task.style.textDecoration = 'line-through'
-            console.log(task)
+
+            const completedTask = document.querySelector('.completed-task-counter')
+            const cntCompletedTask = Number(completedTask.textContent)
+            completedTask.textContent = cntCompletedTask + 1
+
         } else {
+            task.removeAttribute('class')
             task.style.textDecoration = ''
+
+            const completedTask = document.querySelector('.completed-task-counter')
+            const cntCompletedTask = Number(completedTask.textContent)
+            completedTask.textContent = cntCompletedTask - 1
+
         }
     }
 })
@@ -64,18 +90,18 @@ clearAllButton.addEventListener('click', function (e) {
     taskList = Array.from(taskList)
 
     taskList.forEach(function (val) {
-        val.parentElement.removeChild(val)
+
+        const label = val.querySelector('label')
+        if (!(label.style.textDecoration === "")) {
+            val.parentElement.removeChild(val)
+
+            const totalTask = document.querySelector('.total-task-counter')
+            const cntTotalTask = Number(totalTask.textContent)
+            totalTask.textContent = cntTotalTask - 1
+
+            completedTask.textContent = 0
+        }
+
     })
 })
-
-// var checkboxes = document.querySelector('.task-checkbox')
-// console.log(checkboxes)
-// checkboxes.addEventListener('click', function (e) {
-//     let taskList = document.querySelectorAll('.task')
-//     taskList = Array.from(taskList)
-
-//     taskList.forEach(function (val) {
-//         val.parentElement.removeChild(val)
-//     })
-// })
 
