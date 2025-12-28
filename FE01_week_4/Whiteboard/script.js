33c4ab7f94f2ac2a219b3ddf9ddd63aea867b76f
Sah!l <97158384+sahil-1729@users.noreply.github.com>
2025-12-28 19:51:59 +0530

    feat: undo the lines

--- FE01_week_4/Whiteboard/script.js
+++ FE01_week_4/Whiteboard/script.js
@@ -3,18 +3,36 @@ const canvas = document.getElementById("whiteboard-canvas");
 let isDrawing = false;
 let x = 0;
 let y = 0;
+let linesList = []
+let allLines = []
 
+let erase = false
 let drawColor = "black"
 const ctx = canvas.getContext("2d");
 
-function drawLine(context, x1, y1, x2, y2) {
+function drawLine(context, x1, y1, x2, y2, stroke = 4) {
+    if (erase) {
+        stroke = 32
+    }
+
+    linesList.push({
+        drawColor: drawColor,
+        lineWidth: stroke,
+        moveToX: x1,
+        moveToY: y1,
+        lineToX: x2,
+        lineToY: y2,
+    })
+    log(linesList)
+
     context.beginPath();
     context.strokeStyle = drawColor;
-    // context.lineWidth = 1;
+    context.lineWidth = stroke;
     context.moveTo(x1, y1);
     context.lineTo(x2, y2);
     context.stroke();
     context.closePath();
+
 }
 
 function log(msg) {
@@ -39,7 +57,13 @@ if (canvas.getContext) {
 
     canvas.addEventListener('mouseup', (e) => {
         if (isDrawing) {
+            if (erase) {
+                drawLine(ctx, x, y, e.offsetX, e.offsetY, 32);
+            }
             drawLine(ctx, x, y, e.offsetX, e.offsetY);
+
+            allLines.push(linesList)
+
             x = 0;
             y = 0;
             isDrawing = false;
@@ -47,7 +71,7 @@ if (canvas.getContext) {
     })
 
     canvas.addEventListener('mousedown', (e) => {
-
+        linesList = []
         x = e.offsetX;
         y = e.offsetY;
         isDrawing = true;
@@ -73,21 +97,56 @@ const greenBtn = document.querySelector('.green-btn')
 const blueBtn = document.querySelector('.blue-btn')
 const changeColor = document.querySelector('.color-picker')
 const clearBtn = document.querySelector('.clear-btn')
+const eraseBtn = document.querySelector('.erase-btn')
+const undoBtn = document.querySelector('.undo-btn')
+
+undoBtn.addEventListener('click', () => {
+    log('clicked undo')
+    // ctx.restore()
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    allLines.pop()
+    log(allLines)
+    allLines.map((val => {
+
+        val.map((val) => {
+            ctx.beginPath();
+            ctx.strokeStyle = val.drawColor;
+            ctx.lineWidth = val.lineWidth;
+            ctx.moveTo(val.moveToX, val.moveToY);
+            ctx.lineTo(val.lineToX, val.lineToY);
+            ctx.stroke();
+            ctx.closePath();
+        })
+    }))
+})
+
+eraseBtn.addEventListener('click', () => {
+    erase = true
+    drawColor = 'white'
+})
 
 redBtn.addEventListener('click', () => {
+    erase = false
     drawColor = "red"
+    // ctx.save()
 })
 greenBtn.addEventListener('click', () => {
+    erase = false
     drawColor = "green"
 })
 blueBtn.addEventListener('click', () => {
+    erase = false
     drawColor = "blue"
 })
 
 changeColor.addEventListener('change', (e) => {
+    erase = false
     drawColor = e.target.value
 })
 
 clearBtn.addEventListener("click", () => {
+    erase = false
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawColor = 'black'
 })
