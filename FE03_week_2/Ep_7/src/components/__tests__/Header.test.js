020fc8324f471cb99afbd0d51a7d772e6e00feb0
Sah!l <97158384+sahil-1729@users.noreply.github.com>
2026-03-13 20:36:26 +0530

    fix issues to get the test case running

--- FE03_week_2/Ep_7/src/components/__tests__/Header.test.js
+++ FE03_week_2/Ep_7/src/components/__tests__/Header.test.js
@@ -1,10 +1,17 @@
 import Header from "../Header.tsx"
 import {render} from "@testing-library/react"
+import store from "../../utils/store"
+import { Provider } from "react-redux"
+import {StaticRouter} from "react-router-dom/server"
 
 test('Header should load on rendering', () => {
   
     // this will create a small environment using jsdom 
-    const header = render(<Header/>)
+    const header = render(
+    <StaticRouter>
+    <Provider store={store}> <Header  /> </Provider>
+    </StaticRouter>
+)
 
     console.log(header)
 
