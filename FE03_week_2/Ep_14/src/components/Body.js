03c06ded064073e56b27910a63c075ad11b45157
Sah!l <97158384+sahil-1729@users.noreply.github.com>
2026-03-14 23:18:42 +0530

    add routes

--- FE03_week_2/Ep_14/src/components/Body.js
+++ FE03_week_2/Ep_14/src/components/Body.js
@@ -1,10 +1,24 @@
 import React from 'react'
 import Login from './Login'
+import Browse from './Browse'
+import {createBrowserRouter, RouterProvider} from "react-router-dom"
 
 const Body = () => {
+
+  const appRouter = createBrowserRouter([
+    {
+      path: "/",
+      element: <Login/>
+    },
+    {
+      path: "/browse",
+      element: <Browse/>
+    }
+  ])
+
   return (
     <div>
-      <Login/>
+      <RouterProvider router={appRouter}/>
     </div>
   )
 }
