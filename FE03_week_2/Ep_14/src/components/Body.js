0c0a3290328663c0a5796da653a361f58c343efb
Sah!l <97158384+sahil-1729@users.noreply.github.com>
2026-03-20 18:49:30 +0530

    configure redux

--- FE03_week_2/Ep_14/src/components/Body.js
+++ FE03_week_2/Ep_14/src/components/Body.js
@@ -1,24 +1,43 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Login from './Login'
 import Browse from './Browse'
-import {createBrowserRouter, RouterProvider} from "react-router-dom"
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import { useDispatch } from "react-redux";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from '../utils/firebase';
+import { addUser, removeUser } from '../utils/userSlice';
 
 const Body = () => {
 
   const appRouter = createBrowserRouter([
     {
       path: "/",
-      element: <Login/>
+      element: <Login />
     },
     {
       path: "/browse",
-      element: <Browse/>
+      element: <Browse />
     }
   ])
 
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        const { uid } = user.uid;
+        console.log(uid)
+        dispatch(addUser({ uid: uid }))
+      } else {
+        dispatch((removeUser))
+      }
+    });
+  }, [])
+
   return (
     <div>
-      <RouterProvider router={appRouter}/>
+      <RouterProvider router={appRouter} />
     </div>
   )
 }
