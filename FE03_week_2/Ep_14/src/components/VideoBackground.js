adfa9df2218106eea2812602ae9ecbf0f93f962e
Sah!l <97158384+sahil-1729@users.noreply.github.com>
2026-03-22 21:07:26 +0530

    add css to the videoBackground component

--- FE03_week_2/Ep_14/src/components/VideoBackground.js
+++ FE03_week_2/Ep_14/src/components/VideoBackground.js
@@ -8,8 +8,11 @@ const VideoBackground = ({ movieId }) => {
   useMovieTrailer(movieId)
 
   return (
-    <div>
-      <iframe width="560" height="315" src={`https://www.youtube.com/embed/AFuE1LRxm80?si=${trailerId}`} title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" ></iframe>
+    <div className='w-screen'>
+      <iframe
+        className='w-screen aspect-video'
+        src={`https://www.youtube.com/embed/AFuE1LRxm80?si=${trailerId}?&autoplay=1&mute=1`} title="YouTube video player"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" ></iframe>
     </div>
   )
 }
--- FE03_week_2/Ep_14/src/components/VideoTitle.js
+++ FE03_week_2/Ep_14/src/components/VideoTitle.js
@@ -1,18 +1,24 @@
 import React from 'react'
+import playLogo from "../assets/img/netflixPlay.gif"
 
 const VideoTitle = ({ original_title, overview }) => {
   return (
 
-    <div className='pt-32 px-12'>
+    <div className='pt-[20%] px-12 absolute text-white bg-gradient-to-r from-black w-screen aspect-video'>
       <h1 className='text-6xl font-bold'>
         {original_title}
       </h1>
       <p className='py-6 text-lg w-1/4'>
         {overview}
       </p>
-      <div>
-        <button className='bg-gray-500 text-black p-4 px-12 text-lg bg-opacity-50 rounded-md m-2'>▶️ Play</button>
-        <button className='bg-gray-500 text-black p-4 px-12 text-lg bg-opacity-50 rounded-md m-2'>More info</button>
+      <div className='flex col'>
+        <button className='bg-[#fffdff] text-black p-4 px-12 text-md rounded-md m-2 flex row font-bold'>
+          <img src={playLogo} className='bg-black w-8' alt='play logo not found' />
+          <p>
+            Play
+          </p>
+        </button>
+        <button className='bg-gray-500 p-4 px-12 text-md bg-opacity-50 rounded-md m-2'>  More info</button>
       </div>
     </div>
   )
