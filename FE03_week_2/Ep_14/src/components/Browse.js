24c4ddf0bd37c6fb1df9bbb789c4770a604babd6
Sah!l <97158384+sahil-1729@users.noreply.github.com>
2026-03-22 13:01:30 +0530

    fetch movie list from TMDB API and push into movieReducer store

--- FE03_week_2/Ep_14/src/components/Browse.js
+++ FE03_week_2/Ep_14/src/components/Browse.js
@@ -1,14 +1,21 @@
 import React, { useEffect } from 'react'
 import Header from './Header'
 import { API_OPTIONS } from '../utils/constants'
-
+import { useDispatch, useSelector } from 'react-redux'
+import { addNowPlayingMovies } from '../utils/movieSlice'
 const Browse = () => {
+
+  const dispatch = useDispatch()
+  let user = useSelector((store) => console.log('moveis ',store.movies));
+
 
   const getNowPlaying = async () => {
     const data = await fetch('https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1', API_OPTIONS)
 
     const { results } = await data.json()
     console.log(results)
+
+    dispatch(addNowPlayingMovies(results))
   }
 
   useEffect(() => {
