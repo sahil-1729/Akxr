0de6bd6f74afa72e6ee8f0efc1b776d3fefdbc53
Sah!l <97158384+sahil-1729@users.noreply.github.com>
2026-03-22 14:15:25 +0530

    create custom hook

--- FE03_week_2/Ep_14/src/components/Browse.js
+++ FE03_week_2/Ep_14/src/components/Browse.js
@@ -1,33 +1,19 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import Header from './Header'
-import { API_OPTIONS } from '../utils/constants'
 import { useDispatch, useSelector } from 'react-redux'
-import { addNowPlayingMovies } from '../utils/movieSlice'
+import useNowPlayingMovies from '../hooks/useNowPlayingMovies'
+import MainContainer from "./MainContainer"
+import SecondaryContainer from "./SecondaryContainer"
+
 const Browse = () => {
 
-  const dispatch = useDispatch()
-  let user = useSelector((store) => console.log('moveis ',store.movies));
-
-
-  const getNowPlaying = async () => {
-    const data = await fetch('https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1', API_OPTIONS)
-
-    const { results } = await data.json()
-    console.log(results)
-
-    dispatch(addNowPlayingMovies(results))
-  }
-
-  useEffect(() => {
-    getNowPlaying()
-    // return () => {
-
-    // };
-  }, []);
-
+  useNowPlayingMovies()
+  
   return (
     <div>
       <Header />
+      <MainContainer />
+      <SecondaryContainer />
     </div>
   )
 }
---
+++ FE03_week_2/Ep_14/src/hooks/useNowPlayingMovies.js
@@ -0,0 +1,30 @@
+import { useDispatch, useSelector } from "react-redux";
+import { API_OPTIONS } from "../utils/constants";
+import { addNowPlayingMovies } from "../utils/movieSlice";
+import { useEffect } from "react";
+
+const useNowPlayingMovies = () => {
+
+    const dispatch = useDispatch()
+    let user = useSelector((store) => console.log('movies ', store.movies));
+
+
+    const getNowPlaying = async () => {
+        const data = await fetch('https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1', API_OPTIONS)
+
+        const { results } = await data.json()
+        console.log(results)
+
+        dispatch(addNowPlayingMovies(results))
+    }
+
+    useEffect(() => {
+        getNowPlaying()
+        // return () => {
+
+        // };
+    }, []);
+
+}
+
+export default useNowPlayingMovies
